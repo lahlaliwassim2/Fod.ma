e9342689b75e016a7f3e6d53cc1a5f9aef8689d6
lahlaliwassim2 <wlahlali34@gmail.com>
2022-12-14 11:24:18 +0100

    ajouter la partie droite de navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import {AiOutlineMenu , AiOutlineSearch} from 'react-icons/ai'
+import { BsFillCartFill} from 'react-icons/bs'
 function Navbar() {
   return (
     <div className='max-w-[1640px] mx-auto flex  justify-between items-center p-4 '>
@@ -13,9 +14,15 @@ function Navbar() {
             </h1>
         </div>
         {/* Search Input  */}
-        <div>
+        <div className='bg-gray-200 rounded-full flex items-center px-2 w-[200px] sm:w-[400px] lg:w-[500px] '>
             <AiOutlineSearch />
+            <input type="text" className='bg-transparent w-full p-2 focus:outline-none' />
         </div>
+        {/* cart button */}
+        <button className='bg-black  text-white md:flex  hidden items-centerpy-2 '>
+            <BsFillCartFill  size={20} className='mr-2'/>
+            cart
+        </button>
     </div>
   )
 }
