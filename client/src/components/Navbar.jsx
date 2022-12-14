70ad7b5c0b2672ece2812ab194871022b0270e12
lahlaliwassim2 <wlahlali34@gmail.com>
2022-12-14 14:02:31 +0100

    ajouter la function quit permet de affichier la side bar avec le hook useState

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -7,12 +7,12 @@ import { FaWallet,FaUserFriends} from 'react-icons/fa'
 import { MdHelp} from 'react-icons/md'
 import { AiFillTag} from 'react-icons/ai'
 function Navbar() {
-    
+    const [nav,setNav]= useState(false)
   return (
     <div className='max-w-[1640px] mx-auto flex  justify-between items-center p-4 '>
        {/* lEFT SIDE */}
         <div className='flex items-center'>
-            <div className='cursor-pointer '>
+            <div className='cursor-pointer' onClick={()=>{setNav(!nav)}} >
                 <AiOutlineMenu  size={30}/> 
             </div>
             <h1 className='text-2xl sm:text-3xl lg:text-4xl px-2'>
@@ -31,12 +31,16 @@ function Navbar() {
         </button>
 
         {/* mobile menu  */}
+
         {/* Overlay */}
-        <div className='bg-black/80 fixed w-full h-screen z-10 top-0 left-0'></div>
+        {
+            nav ?  <div className='bg-black/80 fixed w-full h-screen z-10 top-0 left-0'></div> : ''
+        }
+       
         
         {/* SIDE BAR MENU */}
-        <div className='fixed top-0 left-0 w-[300px] bg-white duration-300 h-screen z-10  '>
-            <AiOutlineClose  size={30} className="absolute right-4 top-4 cursor-pointer" />
+        <div className={ nav ? 'fixed top-0 left-0 w-[300px] bg-white duration-300 h-screen z-10' : 'fixed top-0 left-[-100%] w-[300px] bg-white duration-300 h-screen z-10'}>
+            <AiOutlineClose  size={30} className="absolute right-4 top-4 cursor-pointer" onClick={()=>{setNav(!nav)}} />
             <h1 className='text-2xl p-4 font-bold'>
                choisir par : 
             </h1>
