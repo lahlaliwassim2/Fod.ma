088631cc8a2ac369b49987f4b97ca6918f7675bd
lahlaliwassim2 <wlahlali34@gmail.com>
2022-12-15 14:46:25 +0100

    ajouter le composant food

--- client/src/components/Food.jsx
+++ client/src/components/Food.jsx
@@ -1,9 +1,20 @@
 import React, { useState } from 'react';
 import { data } from '../data/data.js';
-
+import {AiOutlineClose} from 'react-icons/ai'
 const Food = () => {
   //   console.log(data);
   const [foods, setFoods] = useState(data);
+  const [cardProduct,setCard]= useState(false)
+  const [tempData,setTempData] = useState([])
+
+///findOne 
+const findOne = (image,name,price,description)=>{
+  let tempData = [image,name,price,description]
+    setTempData(item=>[1,...tempData])
+    console.log(tempData)
+    setCard(!cardProduct)
+}
+
 
   //   Filter Type burgers/pizza/etc
   const filterType = (category) => {
@@ -26,14 +37,14 @@ const Food = () => {
   return (
     <div className='max-w-[1640px] m-auto px-4 py-12'>
       <h1 className='text-orange-600 font-bold text-4xl text-center'>
-        Top Rated Menu Items
+       Choisir votres produits 
       </h1>
 
       {/* Filter Row */}
       <div className='flex flex-col lg:flex-row justify-between'>
         {/* Fliter Type */}
         <div>
-          <p className='font-bold text-gray-700'>Filter Type</p>
+          <p className='font-bold text-gray-700'>Filtre par  Type</p>
           <div className='flex justfiy-between flex-wrap'>
             <button
               onClick={() => setFoods(data)}
@@ -70,42 +81,49 @@ const Food = () => {
 
         {/* Filter Price */}
         <div>
-          <p className='font-bold text-gray-700'>Filter Price</p>
+          <p className='font-bold text-gray-700'>Filter par Prix</p>
           <div className='flex justify-between max-w-[390px] w-full'>
             <button
-              onClick={() => filterPrice('$')}
+              onClick={() => filterPrice('35')}
               className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white'
             >
-              $
+              35
             </button>
             <button
-              onClick={() => filterPrice('$$')}
+              onClick={() => filterPrice('45')}
               className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white'
             >
-              $$
+              45
             </button>
             <button
-              onClick={() => filterPrice('$$$')}
+              onClick={() => filterPrice('55')}
               className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white'
             >
-              $$$
+              55
             </button>
             <button
-              onClick={() => filterPrice('$$$$')}
+              onClick={() => filterPrice('65')}
               className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white'
             >
-              $$$$
+              65
+            </button>
+            <button
+              onClick={() => filterPrice('75')}
+              className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white'
+            >
+              75
             </button>
           </div>
         </div>
       </div>
 
       {/* Display foods */}
-      <div className='grid grid-cols-2 lg:grid-cols-4 gap-6 pt-4'>
+      <div className='grid grid-cols-2 lg:grid-cols-4 gap-6 pt-4 cursor-pointer' >
         {foods.map((item, index) => (
-          <div
+          <div 
             key={index}
             className='border shadow-lg rounded-lg hover:scale-105 duration-300'
+            onClick={()=>findOne(item.image,item.name,item.price,item.description)}
           >
             <img
               src={item.image}
@@ -116,14 +134,58 @@ const Food = () => {
               <p className='font-bold'>{item.name}</p>
               <p>
                 <span className='bg-orange-500 text-white p-1 rounded-full'>
-                  {item.price}
+                  {item.price}dh
                 </span>
               </p>
             </div>
           </div>
         ))}
       </div>
+
+
+
+
+           {/* Overlay */}
+           {
+            cardProduct?  <div className='bg-black/80 fixed w-full h-screen z-10 top-0 left-0'></div> : ''
+        }
+       
+        
+        {/* Left Card  */}
+        <div className={ cardProduct? 'fixed top-0 left-0 w-[500px] bg-white duration-300 h-screen z-10' : 'fixed top-0 left-[-100%] w-[300px] bg-white duration-300 h-screen z-10'}>
+            <AiOutlineClose  size={30} className="absolute right-4 top-4 cursor-pointer" onClick={()=>{setCard(!cardProduct)}} />
+            <div className='grid grid-cols-1 gap-6 h-full pt-4'>
+       
+          <div
+  
+            className='border mt-12 shadow-lg rounded-lg hover:scale-10 duration-300'
+          >
+            <img
+              src={tempData[1]}
+              alt=""
+              className='w-full h-[200px] object-cover rounded-t-lg'
+            />
+            <div className='flex justify-between px-2 py-4'>
+              <p className='font-bold'>{tempData[2]}</p>
+              <p>
+                <span className='bg-orange-500 text-white p-1 rounded-full'>
+                 {tempData[3]} dh
+                </span>
+              </p>
+
+            </div>
+            <p>{tempData[4]}</p>
+          </div>
+        
+      </div>
+           
+        </div>
     </div>
+    
+
+
+
+
   );
 };
 
