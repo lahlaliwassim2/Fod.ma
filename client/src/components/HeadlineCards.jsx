6fc58936d6d331c5a3af66e86e0a352d5a999166
lahlaliwassim2 <wlahlali34@gmail.com>
2022-12-15 14:46:47 +0100

    ajouter de style avec tailwind

--- client/src/components/HeadlineCards.jsx
+++ client/src/components/HeadlineCards.jsx
@@ -2,39 +2,35 @@ import React from 'react'
 
 function HeadlineCards() {
   return (
-    <div className='max-w-[1640px] mx-auto p-4 grid md:grid-cols-3 gap-6' >
+    <>
+      <h1 className='text-orange-600 font-bold py-6 text-4xl text-center'>
+        Nous Avons
+      </h1>
+    <div className='max-w-[1640px] mx-auto p-4  grid md:grid-cols-3 gap-6' >
        {/* CARD */} 
+     
        <div className='rounded-xl relative'>
             <div className='absolute w-full h-full bg-black/40 text-white rounded-xl'>
-                <p className='font-bold text-2xl px-2'>Sun's Out,BOGO's Out</p>
-                <p className='px-2'>Through 8/26</p>
-                <button className='border-white bg-white
-                 text-black mx-2 absolute bottom-4'>Order Now</button>
+                <p className='font-bold text-2xl px-2'>Livraison à domecil </p>
             </div>
-            <img  className='max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl' src="https://images.pexels.com/photos/257816/pexels-photo-257816.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+            <img  className='max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl' src="https://images.pexels.com/photos/4393426/pexels-photo-4393426.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
         </div>
 
         <div className='rounded-xl relative'>
             <div className='absolute w-full h-full bg-black/40 text-white rounded-xl'>
-                <p className='font-bold text-2xl px-2'>Sun's Out,BOGO's Out</p>
-                <p className='px-2'>Through 8/26</p>
-                <button className='border-white bg-white
-                 text-black mx-2 absolute bottom-4'>Order Now</button>
+                <p className='font-bold text-2xl px-2'>Plus que 100 produits</p>
             </div>
             <img  className='max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl' src="https://images.pexels.com/photos/1624487/pexels-photo-1624487.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
         </div>
 
         <div className='rounded-xl relative'>
             <div className='absolute w-full h-full bg-black/40 text-white rounded-xl'>
-                <p className='font-bold text-2xl px-2'>Sun's Out,BOGO's Out</p>
-                <p className='px-2'>Through 8/26</p>
-                <button className='border-white bg-white
-                 text-black mx-2 absolute bottom-4'>Order Now</button>
+                <p className='font-bold text-2xl px-2'>les meilleurs chef</p>
             </div>
-            <img  className='max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl' src="https://images.pexels.com/photos/827513/pexels-photo-827513.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+            <img  className='max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl' src="https://images.pexels.com/photos/2977515/pexels-photo-2977515.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
         </div>
     </div>
-  )
+  </>)
 }
 
 export default HeadlineCards
